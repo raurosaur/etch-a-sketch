85416e08d275a3eb1a5cafce46b0096a1712459b
Raunak <raunakb22@gmail.com>
2021-02-27 19:42:18 +0530

    Flood fill function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import domtoimage from "dom-to-image";
+
 const container = document.querySelector("#container");
 const input = document.querySelectorAll("input");
 const randomColor = document.querySelector("#random-color");
@@ -6,20 +7,24 @@ const resetButton = document.querySelector("#reset");
 const borders = document.querySelector("#borders");
 const defBorder = "1px solid black";
 const downloadButton = document.querySelector("#download");
+const floodFillButton = document.querySelector("#flood-fill");
 
 let numGrids = 16;
 let isRandom = false;
 let hasBorder = true;
 let draw = true;
-
+let fillOn = false;
 //Reset
 function reset() {
   container.innerHTML = "";
-  for (let i = 0; i < numGrids * numGrids; ++i) {
+  console.log(numGrids);
+  for (let i = 0, j = 0; i < numGrids * numGrids; ++i) {
     const div = document.createElement("div");
+    div.setAttribute("id", `${Math.floor(j)}-${i % numGrids}`);
     div.setAttribute("tabindex", i + 1);
     div.className += "grid";
     container.appendChild(div);
+    j += 1 / numGrids;
   }
 }
 
@@ -46,7 +51,14 @@ function paint({ target }) {
 //togglePaint
 function togglePaint({ target }) {
   if (target.className === "grid") {
-    if (draw) {
+    if (fillOn) {
+      const [x, y] = target.id.split("-");
+      const targetCol = getComputedStyle(target).backgroundColor;
+      const color = getComputedStyle(document.documentElement).getPropertyValue(
+        "--color"
+      );
+      floodfill(+x, +y, targetCol, color);
+    } else if (draw) {
       target.style.backgroundColor = getComputedStyle(
         document.documentElement
       ).getPropertyValue("--color");
@@ -55,11 +67,35 @@ function togglePaint({ target }) {
   }
   draw = !draw;
 }
+//Flood fill
+function floodfill(x, y, target, newCol) {
+  if(x < 0 || y < 0 || x > numGrids || x > numGrids) return;
+  const cell = document.getElementById(`${x}-${y}`);
+
+  const col = getComputedStyle(cell).backgroundColor;
+  if (col !== target ) return;
+  cell.style.backgroundColor = newCol;
+  floodfill(x,y+1,target, newCol);
+  floodfill(x,y-1,target, newCol);
+  floodfill(x+1,y,target, newCol);
+  floodfill(x-1,y,target, newCol);
+}
+
 /***************Event Listener***************/
 container.addEventListener("click", togglePaint);
+
+/*
+//Mobile Touch Accessibility
+container.addEventListener('touchstart', () => {
+  container.addEventListener('touchmove', paint);
+});
+container.addEventListener('touchend', () => {
+  container.removeEventListener('touchmove', paint);
+});
 container.addEventListener("keydown", (event) => {
   if (event.key === "Enter") paint(event);
 });
+*/
 
 input.forEach((el) =>
   el.addEventListener("input", ({ target }) => {
@@ -76,18 +112,32 @@ input.forEach((el) =>
 
 randomColor.addEventListener("click", ({ target }) => {
   isRandom = !isRandom;
-  target.innerText = `Random Color: ${isRandom ? "On" : "Off"}`;
+  const span = target.querySelector("span");
+  span.innerText = isRandom ? "On" : "Off";
+  span.classList.toggle("off");
+  span.classList.toggle("on");
 });
 
 resetButton.addEventListener("click", reset);
 
 borders.addEventListener("click", ({ target }) => {
   hasBorder = !hasBorder;
-  target.innerText = `Border: ${hasBorder ? "On" : "Off"}`;
+  const span = target.querySelector("span");
+  span.innerText = hasBorder ? "On" : "Off";
+  span.classList.toggle("off");
+  span.classList.toggle("on");
   document.documentElement.style.setProperty(
     "--outline",
     hasBorder ? defBorder : "none"
   );
+});
+
+floodFillButton.addEventListener("click", ({ target }) => {
+  fillOn = !fillOn;
+  const span = target.querySelector("span");
+  span.innerText = fillOn ? "On" : "Off";
+  span.classList.toggle("off");
+  span.classList.toggle("on");
 });
 
 downloadButton.addEventListener("click", () => {
@@ -100,3 +150,10 @@ downloadButton.addEventListener("click", () => {
     link.click();
   });
 });
+
+window.addEventListener('click', () => {
+});
+
+// if(window.innerWidth <= 900){
+//   document.querySelectorAll('button').forEach(el => el.innerHTML = "");
+// }
